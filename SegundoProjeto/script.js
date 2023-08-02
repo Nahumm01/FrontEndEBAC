e039ba005c9d64fb89a503d7806c628f9ed35e4c
Thiago Nahum <nahumm01@outlook.com>
2023-08-01 21:21:40 -0300

    :wrench: chore: validando lógica do campo "Média"

--- SegundoProjeto/script.js
+++ SegundoProjeto/script.js
@@ -1,12 +1,23 @@
 const form = document.getElementById("form");
 const inputNomeAtividade = document.getElementById("nomeAtividade");
 const inputNotaAtividade = document.getElementById("notaAtividade");
-const imgAprove = `<img src="./images/aprovado.png" alt="Emoji Celebrando" />`
-const imgReprove = `<img src="./images/reprovado.png" alt="Emoji Triste" />`
+const imgAprove = `<img src="./images/aprovado.png" alt="Emoji Celebrando" />`;
+const imgReprove = `<img src="./images/reprovado.png" alt="Emoji Triste" />`;
 let linhas = "";
+const atividades = [];
+const notas = [];
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  addLinha();
+  attLinha();
+  attMedias();
+});
+
+function addLinha() {
+  atividades.push(inputNomeAtividade.value);
+  notas.push(parseFloat(inputNotaAtividade.value));
 
   let linha = "<tr>";
   linha += `<td> ${inputNomeAtividade.value}  </td>`;
@@ -18,9 +29,32 @@ form.addEventListener("submit", function (e) {
 
   linhas += linha;
 
-  const corpoTabela = document.querySelector("tbody");
-  corpoTabela.innerHTML = linhas;
-
   inputNomeAtividade.value = "";
   inputNotaAtividade.value = "";
-});
+}
+
+function attLinha() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+
+function attMedias() {
+  const mediaFinal = calcNotaFinal();
+
+  document.getElementById("mediaFinal").innerHTML = mediaFinal;
+  document.getElementById("resultadoFinal").innerHTML = mediaFinal >= 7 ? "Aprovado" : "Reprovado"
+  
+  
+  
+  console.log(somaNotas);
+  console.log(notas);
+}
+
+function calcNotaFinal() {
+  let somaNotas = 0;
+
+  for (let i = 0; i < notas.length; i++) {
+    somaNotas += notas[i];
+  }
+  return somaNotas / notas.length;
+}
