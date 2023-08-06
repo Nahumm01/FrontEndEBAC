ad1ce30809f69cd49a9cfe0872a71808d8b689f8
Thiago Nahum <nahumm01@outlook.com>
2023-08-05 23:32:49 -0300

    :wrench: chore: incluindo validação de nome/tel

--- AgendaContatos/script.js
+++ AgendaContatos/script.js
@@ -17,7 +17,7 @@ function addLinhas() {
   linha += `<td> ${inputTel.value} </td>`;
   linha += "</tr>";
 
-  linhas += linha
+  linhas += linha;
 }
 
 function attBody() {
@@ -27,3 +27,53 @@ function attBody() {
   inputNome.value = "";
   inputTel.value = "";
 }
+
+//ADICIONAR VALIDAÇÃO DO CAMPO STATUS
+
+//ADICIONAR VALIDAÇÃO DE CAMPO COM DOIS NOMES
+function validaNome(nomeCompleto) {
+  const nomeArray = nomeCompleto.split(" ");
+  return nomeArray.length >= 2;
+}
+
+inputNome.addEventListener("input", function (e) {
+  let nomeValido = validaNome(e.target.value);
+  const errorNome = document.querySelector(".nomeError");
+  const btnEnvio = document.querySelector(".btnEnvio")
+  btnEnvio.disabled = false
+
+  if (!nomeValido) {
+    inputNome.style.outline = "1px solid red";
+    errorNome.style.display = "block";
+    btnEnvio.disabled = true
+    btnEnvio.style.cursor = "not-allowed"
+  } else {
+    inputNome.style.outline = "1px solid green";
+    errorNome.style.display = "none";
+    btnEnvio.disabled = false
+    btnEnvio.style.cursor = "pointer"
+  }
+});
+
+//ADICIONAR CONDICIONAL PARA BARRAR NOMES IGUAIS
+
+//MANIPULAR CAMPO TELEFONE PARA JA VIR COM FORMATO BR: (XX)XXXXX-XXXX
+function formatarNumeroTel(numeroTel) {
+  const numeroLimpo = numeroTel.replace(/\D/g, "");
+
+  if (numeroLimpo.length >= 11) {
+    const ddd = numeroLimpo.slice(0, 2);
+    const numberPt1 = numeroLimpo.slice(2, 7);
+    const numberPt2 = numeroLimpo.slice(7);
+
+    return `(${ddd}) ${numberPt1}-${numberPt2}`;
+  } else {
+    return numeroLimpo;
+  }
+}
+
+inputTel.addEventListener("input", function (e) {
+  const numeroTel = e.target.value;
+  const numeroTelFormatado = formatarNumeroTel(numeroTel);
+  e.target.value = numeroTelFormatado;
+});
