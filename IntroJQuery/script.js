44f33571669e768f53f397a6c2052b3e4b1ebf14
Thiago Nahum <nahumm01@outlook.com>
2023-08-10 20:28:20 -0300

    :wrench: chore: manipul jQuery para adicionar imgs

--- IntroJQuery/script.js
+++ IntroJQuery/script.js
@@ -3,11 +3,24 @@ $(document).ready(function () {
     $("form").slideDown();
   });
 
-  $("#btnCancel").click(function(){
-    $("form").slideUp()
-  })
+  $("#btnCancel").click(function () {
+    $("form").slideUp();
+  });
 
   $("form").on("submit", function (e) {
     e.preventDefault();
+
+    const endNovaImg = $("#endImgNova").val();
+    const novoItem = $('<li style="display: none"> </li>');
+    $(`<img src="${endNovaImg}">`).appendTo(novoItem);
+    $(`<div class="overlayImg">
+      <a href="${endNovaImg}" target= "_blank" title="Ver imagem em tamanho real">
+        Ver imagem em tamanho real
+      </a>
+    </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(5000);
+    $("#endImgNova").val("")
   });
 });
