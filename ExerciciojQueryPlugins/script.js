6acb4d60603b2f8e755dc1650d1963fddaa093d1
Thiago Nahum <nahumm01@outlook.com>
2023-08-19 10:42:50 -0300

    :wrench: fix: removendo placeholders

--- ExerciciojQueryPlugins/script.js
+++ ExerciciojQueryPlugins/script.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-    
-    $("#nome").mask("",({
-        placeholder: "Nome + Sobrenome"
-    }))
-    
-    
-    $("#email").mask("",({
-        placeholder: "exemplo@email.com"
-    }))
-    
+       
     $("#tel").mask("(00)00000-0000",({
         placeholder: "(XX)XXXXX-XXXX"
     }))
@@ -16,12 +7,9 @@ $(document).ready(function(){
     $("#CPF").mask("000.000.000-00",({
         placeholder: "123.456.789-00"
     }))
-
-    $("#endereco").mask("",({
-        placeholder: "Rua/Bairro/Cidade"
-    }))
     
     $("#CEP").mask("00000-000",({
         placeholder: "00000-000"
     }))
+
 })
