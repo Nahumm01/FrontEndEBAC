5338b62f700cb24547b1e746bb848817a7a9e2f4
Thiago Nahum <nahumm01@outlook.com>
2023-08-19 10:36:54 -0300

    :wrench: fix: mask plugin jquery

--- ExerciciojQueryPlugins/script.js
+++ ExerciciojQueryPlugins/script.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
+    
+    $("#nome").mask("",({
+        placeholder: "Nome + Sobrenome"
+    }))
+    
+    
+    $("#email").mask("",({
+        placeholder: "exemplo@email.com"
+    }))
+    
+    $("#tel").mask("(00)00000-0000",({
+        placeholder: "(XX)XXXXX-XXXX"
+    }))
+    
     $("#CPF").mask("000.000.000-00",({
-        placeholder: "000.000.000-00"
+        placeholder: "123.456.789-00"
+    }))
+
+    $("#endereco").mask("",({
+        placeholder: "Rua/Bairro/Cidade"
+    }))
+    
+    $("#CEP").mask("00000-000",({
+        placeholder: "00000-000"
     }))
 })
