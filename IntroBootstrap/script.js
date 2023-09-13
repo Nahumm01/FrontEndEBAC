1300f09b9dd3ac3616c28822f83a76a4b2064104
Thiago Nahum <nahumm01@outlook.com>
2023-09-12 21:18:32 -0300

    :wrench: aplicando Validate ao form

--- IntroBootstrap/script.js
+++ IntroBootstrap/script.js
@@ -3,5 +3,25 @@ $(document).ready(function(){
     $("#tel").mask("(00)00000-0000",({
         placeholder: "(XX)XXXXX-XXXX"
     }))
+    
+    $("form").validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            telefone:{
+                required: true,
+            },
+        },
+        messages:{
+            nome:"Por favor insira o nome!",
+            email:"Insira o email corretamente!",
+            telefone:"Insira o seu telefone nos padrões!",
+        },
+    })
      
 })
+
