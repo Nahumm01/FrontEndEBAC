5fc7ebc53b5e4bd3f7ab8a4fa6ac9c537b5ebb0d
Thiago Nahum <nahumm01@outlook.com>
2023-09-18 20:16:51 -0300

    :wrench: fix: corrigindo jQuery

--- IntroBootstrap/script.js
+++ IntroBootstrap/script.js
@@ -1,27 +1,24 @@
-$(document).ready(function(){
-       
-    $("#tel").mask("(00)00000-0000",({
-        placeholder: "(XX)XXXXX-XXXX"
-    }))
-    
-    $("form").validate({
-        rules: {
-            nome: {
-                required: true,
-            },
-            email: {
-                required: true,
-            },
-            telefone:{
-                required: true,
-            },
-        },
-        messages:{
-            nome:"Por favor insira o nome!",
-            email:"Insira o email corretamente!",
-            telefone:"Insira o seu telefone nos padrões!",
-        },
-    })
-     
-})
+$(document).ready(function () {
+  $("#tel").mask("(00)00000-0000", {
+    placeholder: "(XX)XXXXX-XXXX",
+  });
 
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+      },
+      telefone: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: "<h5 class='warn'>Por favor insira o nome!</h5>",
+      email: "<h5 class='warn'>Insira o email corretamente!</h5>",
+      telefone: "<h5 class='warn'>Insira o seu telefone nos padrões!</h5>",
+    },
+  });
+});
