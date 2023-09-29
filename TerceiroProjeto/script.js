cc56aacf37a46e15f3ab198e5ec513323a1c5b6f
Thiago Nahum <nahumm01@outlook.com>
2023-09-29 20:47:42 -0300

    :wrench: implementando Mask/Validate

--- TerceiroProjeto/script.js
+++ TerceiroProjeto/script.js
@@ -4,4 +4,32 @@ $(document).ready(function(){
         placeholder: "(12)12345-1234"
     }))
 
+    $("form").validate({
+        rules: {
+          nome: {
+            required: true,
+          },
+          email: {
+            required: true,
+            email: true,
+          },
+          tel: {
+            required: true,
+          },
+          mensagem: {
+            required: true,
+          },
+          veiculoInteresse: {
+            required: false,
+          },
+        },
+        messages: {
+          nome: "Campo Obrigatório",
+          email:"Campo Obrigatório",
+          tel:"Campo Obrigatório",
+          msg:"Campo Obrigatório",
+        },
+        
+      });
+
 })
