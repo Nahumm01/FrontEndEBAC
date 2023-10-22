7881b46f0e3f78417566b14f9704cdc06ccdc105
Thiago Nahum <nahumm01@outlook.com>
2023-10-22 01:35:46 -0300

    criando tarefas para automação

---
+++ ExercicioGulp/gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imageMin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const obfuscate = require("gulp-obfuscate");
+const sourceMaps = require("gulp-sourcemaps");
+
+function compileSass() {
+  return gulp
+    .src("./source/styles/main.scss")
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+        outputStyle:"compressed"
+    }))
+    .pipe(sourceMaps.write("./maps"))
+    .pipe(gulp.dest("./build/styles"));
+}
+
+function compressImg() {
+  return gulp
+    .src("./source/images/*")
+    .pipe(imageMin())
+    .pipe(gulp.dest("/build/images"));
+}
+
+function compressJavascript() {
+  return gulp
+    .src("./source/scripts/*.js")
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest("./build/scripts"));
+}
+
+exports.default = function () {
+  gulp.watch("./source/images/*"),
+    { ignoreInitial: false },
+    gulp.series(compressImg);
+  gulp.watch("./source/styles/*.scss"),
+    { ignoreInitial: false },
+    gulp.series(compileSass);
+  gulp.watch("./source/scritps/*.js"),
+    { ignoreInitial: false },
+    gulp.series(compressJavascript);
+};
