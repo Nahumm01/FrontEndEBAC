6b5006c4dd627c6ff4f0614519d25eebdb3d5533
Thiago Nahum <nahumm01@outlook.com>
2023-10-29 17:10:41 -0300

    config LESS/SASS compiler/compress

--- IntroGrunt/GruntFile.js
+++ IntroGrunt/GruntFile.js
@@ -7,7 +7,25 @@ module.exports = function (grunt) {
           "main.css": "main.less",
         },
       },
+      production: {
+        options: {
+          compress: true,
+        },
+        files: {
+          "main.min.css": "main.less",
+        },
+      },
     },
+    sass:{
+      options:{
+        style:'compressed'
+      },
+      dist:{
+        files:{
+          'main2.css':'main.scss'
+        }
+      }
+    }
   });
 
   grunt.registerTask("olaGrunt", function () {
@@ -16,9 +34,9 @@ module.exports = function (grunt) {
       console.log("Olá Grunt");
       done();
     }, 4000);
-
-    grunt.loadNpmTasks("grunt-contrib-less");
   });
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-sass");
 
-  grunt.registerTask("default", ["less"]);
+  grunt.registerTask("default", ["less",'sass']);
 };
