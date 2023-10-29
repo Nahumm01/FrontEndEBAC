8e8339d688a44a3e04519a2f12cb6203eec143c6
Thiago Nahum <nahumm01@outlook.com>
2023-10-29 19:48:04 -0300

    Config do Replace para JS em /dev

--- IntroGrunt/GruntFile.js
+++ IntroGrunt/GruntFile.js
@@ -16,67 +16,70 @@ module.exports = function (grunt) {
         },
       },
     },
-    watch:{
-      less:{
-        files:['src/styles/**/*.less'], 
-        tasks:['less:development']
+    watch: {
+      less: {
+        files: ["src/styles/**/*.less"],
+        tasks: ["less:development"],
       },
-      html:{
-        files:['src/index.html'],
-        tasks:['replace:dev']
-      }
-
-    },
-    replace:{
-      dev:{
-        options:{
-          patterns:[
-            {
-              match: 'ENDERECO_DO_CSS',
-              replacement:'./styles/main.css'
-            }
-          ]
-        },
-        files:[{
-          expand: true,
-          flatten: true,
-          src:['src/index.html'],
-          dest:'dev/'
-
-        }]
+      html: {
+        files: ["src/index.html"],
+        tasks: ["replace:dev"],
       },
-      dist:{
-        options:{
-          patterns:[
+    },
+    replace: {
+      dev: {
+        options: {
+          patterns: [
             {
-              match: 'ENDERECO_DO_CSS',
-              replacement:'./styles/main.min.css'
-            }
-          ]
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.css",
+            },
+            {
+              match: "ENDERECO_DO_JS",
+              replacement: "../src/scripts/main.js",
+            },
+          ],
         },
-        files:[{
-          expand: true,
-          flatten: true,
-          src:['prebuild/index.html'],
-          dest:'dist/'
-
-        }]
-      }
-    },
-    htmlmin:{
-      dist:{
-        options:{
-          removeComments:true,
-          collapseWhitespace:true,
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["src/index.html"],
+            dest: "dev/",
+          },
+        ],
+      },
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: "ENDERECO_DO_CSS",
+              replacement: "./styles/main.min.css",
+            },
+          ],
         },
-        files:{
-          'prebuild/index.html':'src/index.html'
-        }
-      }
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ["prebuild/index.html"],
+            dest: "dist/",
+          },
+        ],
+      },
     },
-    clean:[
-      'prebuild'
-    ]
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          "prebuild/index.html": "src/index.html",
+        },
+      },
+    },
+    clean: ["prebuild"],
   });
 
   grunt.loadNpmTasks("grunt-contrib-less");
@@ -85,10 +88,11 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-htmlmin");
   grunt.loadNpmTasks("grunt-contrib-clean");
 
-
-
-
   grunt.registerTask("default", ["watch"]);
-  grunt.registerTask("build", ["less:production",'htmlmin:dist','replace:dist', 'clean']);
-
+  grunt.registerTask("build", [
+    "less:production",
+    "htmlmin:dist",
+    "replace:dist",
+    "clean",
+  ]);
 };
